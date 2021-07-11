14d9f27fcbd62762f99efbb103d9f12936a55067
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2021-07-11 12:09:40 +0200

    save last index and start from there

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+process.env.NTBA_FIX_319 = 1;
+const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 const schedule = require('node-schedule');
@@ -10,6 +12,8 @@ const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
+const LASTINDEXFILE = 'lastIndex.txt';
+
 function getRows(callback) {
     sheets.spreadsheets.values.get({
         spreadsheetId: process.env.SPREADSHEET_ID,
@@ -21,15 +25,30 @@ function getRows(callback) {
 }
 
 console.log('Bot has started');
-let index = 0;
-schedule.scheduleJob('0 * * * *', () => getRows(main))
-//getRows(main);
+let index;
+if (process.argv.length > 2 && process.argv[2].toLocaleLowerCase() == '--start') {
+    index = 0;
+}
+else {
+    try {
+        index = parseInt(fs.readFileSync(LASTINDEXFILE, 'utf8'), 10);
+    }
+    catch (e) {
+        index = 0;
+    }
+}
 
-//setInterval(() => {index++; getRows(main)}, 1000 * 60 * 60)
+schedule.scheduleJob('0 * * * *', () => getRows(main))
 
 function main(phrases) {
     if (index >= phrases.length) index = index % phrases.length;
     console.log(`[${new Date().toLocaleString()}] ${phrases[index][0]}`)
     bot.sendMessage(CHAT_ID, phrases[index][0]);
     index++;
+    try {
+        fs.writeFileSync(LASTINDEXFILE, `${index}`);
+    }
+    catch (e) {
+        console.log("Error: Couldn't write to " + LASTINDEXFILE);
+    }
 }
