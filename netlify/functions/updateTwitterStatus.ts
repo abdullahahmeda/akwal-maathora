1710b3d22b0e51f0b566506586b33b08b45b4a8c
Abdullah <abdullah.ahmed.a2000@gmail.com>
2023-11-17 15:53:02 +0200

    lets go

--- netlify/functions/updateTwitterStatus.ts
+++ netlify/functions/updateTwitterStatus.ts
@@ -8,29 +8,33 @@ import { getPhrases } from "../../services/phrases";
 import { updateTwitterStatus } from "../../utils/updateTwitterStatus";
 import { getTwitterIndex, updateTwitterIndex } from "../../services/settings";
 
-const myHandler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext,
-) => {
-  let twitterIndex = await getTwitterIndex();
-  const phrases = (await getPhrases()) as string[][];
-  if (twitterIndex >= phrases.length) {
-    twitterIndex = twitterIndex % phrases.length;
-  }
-  const status = phrases?.[twitterIndex]?.[0] || "";
-  if (status.trim() !== "") {
-    try {
-      await updateTwitterStatus(status);
-    } catch (error) {
-      // Just update the index
-    }
-  }
+// const myHandler: Handler = async (
+//   event: HandlerEvent,
+//   context: HandlerContext,
+// ) => {
+//   let twitterIndex = await getTwitterIndex();
+//   const phrases = (await getPhrases()) as string[][];
+//   if (twitterIndex >= phrases.length) {
+//     twitterIndex = twitterIndex % phrases.length;
+//   }
+//   const status = phrases?.[twitterIndex]?.[0] || "";
+//   if (status.trim() !== "") {
+//     try {
+//       await updateTwitterStatus(status);
+//     } catch (error) {
+//       // Just update the index
+//     }
+//   }
+//
+//   await updateTwitterIndex(twitterIndex + 1);
+//
+//   return { statusCode: 200 };
+// };
+//
+// const handler = schedule("@hourly", myHandler);
+//
+// export { handler };
 
-  await updateTwitterIndex(twitterIndex + 1);
-
+export const handler = async (event: HandlerEvent, context: HandlerContext) => {
   return { statusCode: 200 };
 };
-
-const handler = schedule("@hourly", myHandler);
-
-export { handler };
