1069479c91c212f22b79a00682b838fc6e7463af
Abdullah <abdullah.ahmed.a2000@gmail.com>
2023-11-19 23:52:51 +0200

    asd

--- netlify/functions/updateTwitterStatus.ts
+++ netlify/functions/updateTwitterStatus.ts
@@ -2,39 +2,39 @@ import {
   Handler,
   HandlerEvent,
   HandlerContext,
-  // schedule,
+  schedule,
 } from "@netlify/functions";
 import { getPhrases } from "../../services/phrases";
 import { updateTwitterStatus } from "../../utils/updateTwitterStatus";
 import { getTwitterIndex, updateTwitterIndex } from "../../services/settings";
 
-// const myHandler: Handler = async (
-//   event: HandlerEvent,
-//   context: HandlerContext,
-// ) => {
-//   let twitterIndex = await getTwitterIndex();
-//   const phrases = (await getPhrases()) as string[][];
-//   if (twitterIndex >= phrases.length) {
-//     twitterIndex = twitterIndex % phrases.length;
-//   }
-//   const status = phrases?.[twitterIndex]?.[0] || "";
-//   if (status.trim() !== "") {
-//     try {
-//       await updateTwitterStatus(status);
-//     } catch (error) {
-//       // Just update the index
-//     }
-//   }
-//
-//   await updateTwitterIndex(twitterIndex + 1);
-//
-//   return { statusCode: 200 };
-// };
-//
-// const handler = schedule("@hourly", myHandler);
-//
-// export { handler };
+const myHandler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext,
+) => {
+  let twitterIndex = await getTwitterIndex();
+  const phrases = (await getPhrases()) as string[][];
+  if (twitterIndex >= phrases.length) {
+    twitterIndex = twitterIndex % phrases.length;
+  }
+  const status = phrases?.[twitterIndex]?.[0] || "";
+  if (status.trim() !== "") {
+    try {
+      await updateTwitterStatus(status);
+    } catch (error) {
+      // Do nothing
+    }
+  }
 
-export const handler = async (event: HandlerEvent, context: HandlerContext) => {
+  await updateTwitterIndex(twitterIndex + 1);
+
   return { statusCode: 200 };
 };
+
+const handler = schedule("@hourly", myHandler);
+
+export { handler };
+
+// export const handler = async (event: HandlerEvent, context: HandlerContext) => {
+//   return { statusCode: 200 };
+// };
