5f0bd261018308998e08e94e427aa0a68bd918d5
Abdullah <abdullah.ahmed.a2000@gmail.com>
2024-04-18 13:25:58 +0200

    good

--- utils/db.ts
+++ utils/db.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@libsql/client";
-import { fetch } from "undici";
 
 export const db = createClient({
-  fetch,
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
